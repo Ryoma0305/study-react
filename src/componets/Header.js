322aca53b46b9954136c87b0e258a62943ae970b
Ryoma0305 <ryomaaa0602@gmail.com>
2023-02-06 13:45:50 +0900

    headerリンク更新

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export function Header() {
   return (
     <header>
       <h1>Header</h1>
+      <div>
+        <Link href="/">TOP</Link>
+        <Link href="/about">About</Link>
+      </div>
     </header>
   );
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Header } from "../componets/Header";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
@@ -14,6 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
